5a1277ea4c1c9891472780e9841fd5e40b77a769
aniram112 <masyakmarina@gmail.com>
2019-11-02 20:39:58 +0300

    all done i hope

---
+++ Assets/Death.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+    public GameObject death;
+    public GameObject end;
+    public GameObject pau;
+    public GameObject joy; 
+    public GameObject but;
+    public GameObject task;
+
+    public void Dead(){
+        death.SetActive(true);
+        but.SetActive(false);
+        joy.SetActive(false);
+        pau.SetActive(false);
+        task.SetActive(false);
+        Time.timeScale = 0f;
+
+    }
+
+    public void End()
+    {
+        end.SetActive(true);
+        but.SetActive(false);
+        joy.SetActive(false);
+        task.SetActive(false);
+        pau.SetActive(false);
+        Time.timeScale = 0f;
+
+    }
+}
