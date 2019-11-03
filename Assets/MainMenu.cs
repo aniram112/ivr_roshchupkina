960572266e5a7695328c47958c3e0d43e86e5ee7
aniram112 <masyakmarina@gmail.com>
2019-11-03 19:58:46 +0300

    i hate git

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,19 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public Trophies locked;
     public void PlayGame(){
-        if (locked.GetLock(0) == 0 || locked.GetLock(4) == 1) {
-            Time.timeScale = 1f;
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
-
-        else 
-        {
-            Time.timeScale = 1f;
-            SceneManager.LoadScene("Level1");
-        }
-       
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
 }
--- Assets/Pause.cs
+++ Assets/Pause.cs
@@ -5,13 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 {
-    public Trophies locked;
-    public Transform player;
-    public Transform cam;
-    public Transform qte;
-
-
-
+    public GameObject guard;
+    public GameObject player;
     public void Menu()
     {
         SceneManager.LoadScene("MainMenu");
@@ -22,15 +17,6 @@ public class Pause : MonoBehaviour
     public void Continue()
     {
         Time.timeScale = 1f;
-    }
-    public void Restart(){
-        if (locked.GetLock(1)==0) { SceneManager.LoadScene("Level1"); }
-        else if (locked.GetLock(2) == 0 && locked.GetLock(1) == 1) { player.transform.position = cam.transform.position; }
-        else if (locked.GetLock(3) == 0 && locked.GetLock(2) == 1 && locked.GetLock(1) == 1) { player.transform.position = qte.transform.position; }
-        Time.timeScale = 1f;
-
-
-
     }
 
 
